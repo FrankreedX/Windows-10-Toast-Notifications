2ef550b41eacde761cd420dadd5b47d7c888e2f6
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-10-23 11:40:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
-
 from operator import attrgetter
 from os import path
-
-from pip.req import parse_requirements
 from setuptools import setup
 
 def read(fname):
     return open(path.join(path.dirname(__file__), fname)).read()
 
-
 def from_here(relative_path):
     return path.join(path.dirname(__file__), relative_path)
 
-
-requirements_txt = list(map(str, map(
-    attrgetter("req"),
-    parse_requirements(from_here("requirements.txt"), session="")
-)))
+def get_requirements(filename='requirements.txt'):
+    """Read requirements from requirements.txt"""
+    requirements = []
+    if path.exists(filename):
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    if line.startswith('-e '):
+                        requirements.append(line.split('#egg=')[1])
+                    else:
+                        requirements.append(line)
+    return requirements
 
 setup(
     name="win10toast",
     version="0.9",
-    install_requires=requirements_txt,
+    install_requires=get_requirements(from_here("requirements.txt")),
     packages=["win10toast"],
     license="BSD",
     url="https://github.com/jithurjacob/Windows-10-Toast-Notifications",
-    download_url = 'https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.9',
+    download_url='https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.9',
     description=(
         "An easy-to-use Python library for displaying "
         "Windows 10 Toast Notifications"
@@ -36,6 +40,7 @@ setup(
         'win10toast': ['data/*.ico'],
     },
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",  # Added to properly render README on PyPI
     author="Jithu R Jacob",
     author_email="jithurjacob@gmail.com",
     classifiers=[
@@ -45,4 +50,5 @@ setup(
         'Environment :: Win32 (MS Windows)',
         "License :: OSI Approved :: MIT License",
     ],
+    python_requires=">=3.6",  # Added to specify Python version requirement
 )
